f5ad345181c8b78f1e24494a0eb31271976b17e6
Angelina S <Angelinko.lol@gmail.com>
2020-09-22 12:24:08 +0300

    addHW2 + jar

---
+++ My_project_HW_Angelina_hw2/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import ru.pflb.mq.dummy.exception.DummyException;
+import ru.pflb.mq.dummy.implementation.ConnectionImpl;
+import ru.pflb.mq.dummy.interfaces.Connection;
+import ru.pflb.mq.dummy.interfaces.Destination;
+import ru.pflb.mq.dummy.interfaces.Producer;
+import ru.pflb.mq.dummy.interfaces.Session;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException, DummyException, IOException {
+
+        try (Connection connection = new ConnectionImpl();
+             Session session = connection.createSession(true);
+             BufferedReader reader = new BufferedReader(
+                     new InputStreamReader(
+                             new FileInputStream("/My_project_HW_Angelina_hw2/messages.dat"), StandardCharsets.UTF_8))) {
+            Destination destination = session.createDestination("que01");
+            Producer producer = session.createProducer(destination);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Thread.sleep(2000);
+                producer.send(line);
+            }
+        }
+    }
+}
+
